86e346fac92ac9a63b33633965bcf62366f4f4f1
AlanGuastavino <127367062+AlanGuastavino@users.noreply.github.com>
2023-10-02 20:02:16 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,139 +1,108 @@
+let modelosMonitores = []; 
 
-//Entrega Final - Guastavino
-
-fetch('modelosMonitores.json')
-  .then(response => response.json())
-  .then(data => {
-    const modelosMonitores = data;
-
-    function cargarOpciones(selectId, opciones) {
-      const select = document.getElementById(selectId);
-      select.innerHTML = '';
-      opciones.forEach((opcion) => {
-        const option = document.createElement('option');
-        option.value = opcion;
-        option.textContent = opcion;
-        select.appendChild(option);
-      });
-    }
-
- 
-    function calcularCostoMonitor(marca, modelo, tipoPanel, pulgadas, frecuencia) {
-      const monitor = modelosMonitores.find(
-        (marcaMonitor) => marcaMonitor.marca === marca
-      );
-
-      if (monitor) {
-        const modeloMonitor = monitor.modelos.find(
-          (modeloMonitor) =>
-            modeloMonitor.nombre === modelo &&
-            modeloMonitor.tipoPanel === tipoPanel &&
-            modeloMonitor.pulgadas.includes(pulgadas) &&
-            frecuencia in modeloMonitor.frecuencia
-        );
-
-        if (modeloMonitor) {
-          return modeloMonitor.frecuencia[frecuencia];
-        }
-      }
-
-      return 0;
-    }
-
-    let totalCarrito = 0;
-
-    window.onload = function() {
-      const marcaSelect = document.getElementById('marca');
-      const modeloSelect = document.getElementById('modelo');
-      const tipoPanelSelect = document.getElementById('tipoPanel');
-      const pulgadasSelect = document.getElementById('pulgadas');
-      const frecuenciaSelect = document.getElementById('frecuencia');
-      const calcularButton = document.getElementById('calcular');
-      const detallesCompraDiv = document.getElementById('detalles-compra');
-      const carritoLista = document.getElementById('carrito-lista');
-
-      marcaSelect.addEventListener('change', function() {
-        const marcaSeleccionada = marcaSelect.value;
-        const modelosMarcaSeleccionada = modelosMonitores.find(
-          (marcaMonitor) => marcaMonitor.marca === marcaSeleccionada
-        ).modelos;
-        
-        cargarOpciones('modelo', modelosMarcaSeleccionada.map((modeloMonitor) => modeloMonitor.nombre));
-        cargarOpciones('tipoPanel', [modelosMarcaSeleccionada[0].tipoPanel]);
-        cargarOpciones('pulgadas', modelosMarcaSeleccionada[0].pulgadas);
-        const frecuencias = Object.keys(modelosMarcaSeleccionada[0].frecuencia);
-        cargarOpciones('frecuencia', frecuencias);
+function cargarModelosMonitores() {
+  fetch('modelosMonitores.json')
+    .then(response => response.json())
+    .then(data => {
+      modelosMonitores = data;
+      const marcas = [...new Set(data.map(item => item.marca))];
+      marcas.forEach((marca) => {
+        const option = document.createElement("option");
+        option.value = marca;
+        option.textContent = marca;
+        marcaSelect.appendChild(option);
       });
 
-      modeloSelect.addEventListener('change', function() {
-        const marcaSeleccionada = marcaSelect.value;
-        const modeloSeleccionado = modeloSelect.value;
-        const modeloMonitor = modelosMonitores
-          .find((marcaMonitor) => marcaMonitor.marca === marcaSeleccionada)
-          .modelos.find((modeloMonitor) => modeloMonitor.nombre === modeloSeleccionado);
-
-        cargarOpciones('tipoPanel', [modeloMonitor.tipoPanel]);
-        cargarOpciones('pulgadas', modeloMonitor.pulgadas);
-        const frecuencias = Object.keys(modeloMonitor.frecuencia);
-        cargarOpciones('frecuencia', frecuencias);
+      const modelosPrimeraMarca = data.filter(item => item.marca === marcas[0]);
+      modelosPrimeraMarca.forEach((modelo) => {
+        const option = document.createElement("option");
+        option.value = modelo.modelo;
+        option.textContent = modelo.modelo;
+        modeloSelect.appendChild(option);
       });
+    })
+    .catch(error => {
+      console.error('Error al cargar el archivo JSON', error);
+    });
+}
 
-      calcularButton.addEventListener('click', function() {
-        const marca = marcaSelect.value;
-        const modelo = modeloSelect.value;
-        const tipoPanel = tipoPanelSelect.value;
-        const pulgadas = pulgadasSelect.value;
-        const frecuencia = frecuenciaSelect.value;
+window.onload = function () {
+  const marcaSelect = document.getElementById("marca"); 
+  const modeloSelect = document.getElementById("modelo");
 
-        const costo = calcularCostoMonitor(marca, modelo, tipoPanel, pulgadas, frecuencia);
+  cargarModelosMonitores();
 
-        if (costo > 0) {
-          const detallesMonitor = {
-            marca: marca,
-            modelo: modelo,
-            tipoPanel: tipoPanel,
-            pulgadas: pulgadas,
-            frecuencia: frecuencia,
-            costo: costo
-          };
+  const tipoPanelSelect = document.getElementById("tipoPanel");
+  const pulgadasSelect = document.getElementById("pulgadas");
+  const frecuenciaSelect = document.getElementById("frecuencia");
+  const calcularButton = document.getElementById("calcular");
+  const detallesCompraDiv = document.getElementById("detalles-compra");
+  const resultadoDiv = document.getElementById("resultado");
 
-          localStorage.setItem('detallesMonitor', JSON.stringify(detallesMonitor));
+  marcaSelect.addEventListener("change", function () {
+    modeloSelect.innerHTML = "";
+    const modelos = modelosMonitores.filter((item) => item.marca === marcaSelect.value);
+    modelos.forEach((modelo) => {
+      const option = document.createElement("option");
+      option.value = modelo.modelo;
+      option.textContent = modelo.modelo;
+      modeloSelect.appendChild(option);
+    });
+  });
 
-          detallesCompraDiv.innerHTML = `
-            <h2>Detalles de la Compra</h2>
-            <p>Marca: ${marca}</p>
-            <p>Modelo: ${modelo}</p>
-            <p>Tipo de Panel: ${tipoPanel}</p>
-            <p>Pulgadas: ${pulgadas} pulgadas</p>
-            <p>Frecuencia: ${frecuencia}</p>
-            <p>Costo: $${costo.toFixed(2)}</p>
-            <button id="comprar">Agregar al carrito de compras</button>
+  calcularButton.addEventListener("click", function () {
+    const marca = marcaSelect.value;
+    const modelo = modeloSelect.value;
+    const tipoPanel = tipoPanelSelect.value;
+    const pulgadas = pulgadasSelect.value;
+    const frecuencia = frecuenciaSelect.value;
+
+    const monitor = modelosMonitores.find(
+      (item) =>
+        item.marca === marca &&
+        item.modelo === modelo &&
+        item.tipoPanel === tipoPanel &&
+        item.pulgadas === pulgadas &&
+        frecuencia in item.frecuencia
+    );
+
+    if (monitor) {
+      const costo = monitor.frecuencia[frecuencia];
+
+      const detallesMonitor = {
+        marca: marca,
+        modelo: modelo,
+        tipoPanel: tipoPanel,
+        pulgadas: pulgadas,
+        frecuencia: frecuencia,
+        costo: costo,
+      };
+
+      localStorage.setItem("detallesMonitor", JSON.stringify(detallesMonitor));
+
+      detallesCompraDiv.innerHTML = `
+          <h2>Detalles de la Compra</h2>
+          <p>Marca: ${marca}</p>
+          <p>Modelo: ${modelo}</p>
+          <p>Tipo de Panel: ${tipoPanel}</p>
+          <p>Pulgadas: ${pulgadas} pulgadas</p>
+          <p>Frecuencia: ${frecuencia}</p>
+          <p>Costo: $${costo}</p>
+          <button id="comprar">COMPRAR</button>
+        `;
+
+      const comprarButton = document.getElementById("comprar");
+      comprarButton.addEventListener("click", function () {
+        resultadoDiv.innerHTML = `
+            <p>¡Compra realizada con éxito!</p>
           `;
-
-          const comprarButton = document.getElementById('comprar');
-          comprarButton.addEventListener('click', function() {
-            carritoLista.innerHTML += `
-              <li>
-                <span>${marca} ${modelo}</span>
-                <span>Tipo de Panel: ${tipoPanel}</span>
-                <span>Pulgadas: ${pulgadas} pulgadas</span>
-                <span>Frecuencia: ${frecuencia}</span>
-                <span>Costo: $${costo.toFixed(2)}</span>
-              </li>
-            `;
-
-           
-            totalCarrito += costo;
-            document.getElementById('total-carrito').textContent = `$${totalCarrito.toFixed(2)}`;
-
-           
-          });
-        } else {
-          detallesCompraDiv.innerHTML = `
-            <p>El monitor seleccionado no se encuentra disponible en nuestras opciones.</p>
-          `;
-         
-        }
       });
-    };
-  })
+    } else {
+      detallesCompraDiv.innerHTML = `
+          <p>El monitor seleccionado no se encuentra disponible en nuestras opciones.</p>
+        `;
+      resultadoDiv.innerHTML = "";
+    }
+  });
+};
+
